b380a63d574c3ef3a126ba3b738fa8752ac286fc
ahmetkadakal <ahmetkadakal464@gmail.com>
2024-08-30 16:11:42 +0300

    Create lesson_Two_Veriables.dart

---
+++ lesson_Two_Veriables.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  int age = 31;
+  double average = 54.518;
+  num year = 1995;
+
+  print(age);
+  print(average);
+  print(year);
+
+  String name = "ahmet";
+  print(name);
+
+  bool isItMan = true;
+  print(isItMan);
+
+  var school = "middle school";
+  var schoolNumber = 1655;
+  var doYouHaveCat = true;
+
+  print(school);
+  print(schoolNumber);
+  print(doYouHaveCat);
+}
