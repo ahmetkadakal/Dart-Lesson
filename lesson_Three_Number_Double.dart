c1a2f524a94db49ea415e4984d292d0f127f0ee4
ahmetkadakal <ahmetkadakal464@gmail.com>
2024-08-31 19:18:56 +0300

    Update lesson_Three_Number_Double.dart

--- lesson_Three_Number_Double.dart
+++ lesson_Three_Number_Double.dart
@@ -1,8 +1,28 @@
 void main(List<String> args) {
   num a = 321;
   double b = 252.25;
+  int c = 69;
+
+  var abc = 35;
+
   /*
   num is for full number
   doble is for decimal number 
   */
+
+  print(a);
+  print(b);
+  print(c);
+  print(abc);
+
+  //null safety
+  int number;
+  number = 31;
+  print(number);
+
+  double? n1 = null;
+  print(n1);
+
+  int hexadecimalNumber = 0xAABBCC;
+  print(hexadecimalNumber);
 }
