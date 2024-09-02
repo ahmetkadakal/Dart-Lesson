2ad194e6dbc1d417deecc43267ae813ac5514874
ahmetkadakal <ahmetkadakal464@gmail.com>
2024-09-02 11:28:36 +0300

    Create lesson_Eight_Switch_To_Use.dart

---
+++ lesson_Eight_Switch_To_Use.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  //only int and String value to use
+
+  String pointValue = "BA";
+
+  switch (pointValue) {
+    case "AA":
+      print("You are okey");
+      break;
+    case "BA":
+      print("You are bad");
+    default:
+      print("Very very bad");
+  }
+}
