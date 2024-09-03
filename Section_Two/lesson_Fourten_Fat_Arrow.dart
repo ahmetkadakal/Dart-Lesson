0f97684c9ac9eb1a187f4649355c72e3072edbc1
ahmetkadakal <ahmetkadakal464@gmail.com>
2024-09-03 13:11:17 +0300

    Update lesson_Fourten_Fat_Arrow.dart

--- Section_Two/lesson_Fourten_Fat_Arrow.dart
+++ Section_Two/lesson_Fourten_Fat_Arrow.dart
@@ -1,1 +1,16 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  print(maxFind(4, 8, 6));
+  print(minFind(89, 65, 233));
+}
+
+int maxFind(int a, int b, int c) => a > b && a > c
+    ? a
+    : b > c && b > a
+        ? b
+        : c;
+
+int minFind(int a, int b, int c) => a < b && a < c
+    ? a
+    : b < a && b < c
+        ? b
+        : c;
